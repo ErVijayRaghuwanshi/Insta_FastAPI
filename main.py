63f4bbba2a462ef7960fe77df29de7dd0fc6279e
Vijay Raghuwanshi <37011729+ErVijayRaghuwanshi@users.noreply.github.com>
2022-12-30 17:56:20 +0530

    Update main.py
    removing caching support

--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 from uvicorn import run
-from fastapi import FastAPI, Response
+from fastapi import FastAPI
 import requests
-import requests_cache
-import requests_random_user_agent
 
-requests_cache.install_cache("insta_cache", backend='sqlite', expire_after=60*60*24*30, match_headers=False, cache_control=False)
-
-# ========================== scraping the data ========================== 
-def UserInfo(user_name: str) -> dict:
-    reqUrl = f"https://www.instagram.com/api/v1/users/web_profile_info/?username={user_name}"
-
-    headersList = {
-                    "x-ig-app-id": "936619743392459",
-                }
-
-    payload = f"username={user_name}"
-    try:
-        resp = requests.get(reqUrl, data=payload,  headers=headersList)
-        from_cache = resp.from_cache
-        if resp.ok:
-            data = resp.json()
-        else: 
-            data = {"message": f"invalid user_name: {user_name}"}
-    except Exception as e:
-        data = {"message": f"invalid user_name: {user_name}", "error": str(e)}
-    # return response.json()
-    
-    return data, from_cache
-# ========================== scraping the data ========================== 
 
 app = FastAPI(
     title="Instagram FastAPI",
@@ -39,14 +13,24 @@ app = FastAPI(
 )
 
 @app.get("/getUserInfo/{user_name}", description="Get the user info of Instagram user by it's name like \"gima_ashi\"", tags=["Instagram"])
-def read_root(response: Response,user_name):
-    info, is_from_cache = UserInfo(user_name=str(user_name).strip())
-    response.headers["from-cache"] = str(is_from_cache)
-    
-    return {"message": info}
+def read_root(user_name):
+    data = {}
+
+    reqUrl = f"https://www.instagram.com/api/v1/users/web_profile_info/?username={user_name}"
+    headersList = {
+    "x-ig-app-id": "936619743392459" 
+    }
+    try:
+        response = requests.get(reqUrl, headers=headersList)
+        if response.ok:
+            data = response.json()
+        else:
+            data = {"message": f"invalid user_name: {user_name}", "status_code": response.status_code}
+    except Exception as e:
+        data = {"message": "error", "exception": str(e)}
+
+    return {"message": data}
 
 
 if __name__ == "__main__":
-    # import webbrowser
-    # webbrowser.open("http://127.0.0.1:5500")
-    run(app, host="127.0.0.1", port=5500)
+    run("main:app", host="0.0.0.0")
